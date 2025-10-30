b8a65becb100b7a8bca5d13ca09870038eca82ec
Umaru Bayoh <bayohumau66@gmail.com>
2025-10-30 15:11:21 +0000

    Refactor addProductToCart function in product.js to fix cart broken activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// Modify code that fixed cart broken
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get cart array of items from local storage if null
+  const cartItems = getLocalStorage("so-cart") || [];
+  // Push the items into the array object
+  cartItems.push(product);
+  // Set the local storage
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
