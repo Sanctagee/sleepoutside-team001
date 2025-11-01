549e984ceafdbb4fff06896870204124b9e3e602
Bryan Mino Lenovo <bryan_royce1997@hotmail.com>
2025-11-01 01:12:15 -0500

    Fixing the code od the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+
+  // First, We need to obtain the last element saved in the variable in Local Storage called "so-cart"
+  const lastItems = getLocalStorage("so-cart") || [];
+
+  lastItems.push(product);
+
+  setLocalStorage("so-cart", lastItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
