09172fc7aff526132e01a4bee7dda22fc42432bd
Umaru Bayoh <bayohumau66@gmail.com>
2025-11-07 15:27:38 +0000

    wored on the week 2 team project

--- src/js/product.js
+++ src/js/product.js
@@ -1,32 +1,14 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+// Import this new getParam function into product.js.
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-// To create the function that will add products to cart:
-function addProductToCart(product) {
-  // Always get cart items or initialize as empty array
-  let cartItems = getLocalStorage("so-cart");
-  
-  // In a condition when it's not an array, start fresh with empty array
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-  
-  // To add new product to cart which is now an array
-  cartItems.push(product);
-  // To save updated cart back to local storage
-  setLocalStorage("so-cart", cartItems);
-}
+// Test the getParam function in product.js to see if the productId displays in the URL when a product is clicked.
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 
-// handler for add to cart button
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// attach event listener to add to cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,18 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+// this function extracts and returns the value of a query parameter from the page's URL
+export function getParam(param) {
+  // get the query string part of the current URL (the part after the ?)
+  const queryString = window.location.search;
+
+  // create a URLSearchParams object making it convenient to work with the parameters
+  const urlParams = new URLSearchParams(queryString);
+
+  // retrieves that value of the named parameter, in this case param
+  const productCategory = urlParams.get(param);
+  // return that value, in this case, the product category
+  return productCategory;
+}
+
