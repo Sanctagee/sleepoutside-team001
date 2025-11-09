af0d515a98080acf43589630c5a59d3abace740b
Bryan Mino Lenovo <bryan_royce1997@hotmail.com>
2025-11-09 01:47:20 -0500

    Changes

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,11 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  // First, We need to obtain the last element saved in the variable in Local Storage called "so-cart"
-  const lastItems = getLocalStorage("so-cart") || [];
+const productId = getParam("product");
 
-  lastItems.push(product);
+const details = new ProductDetails(productId, dataSource);
 
-  setLocalStorage("so-cart", lastItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+details.init();
