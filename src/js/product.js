9cd6a01a4ab8d5a54b051780cacdac0dc6b96d26
teory01 <youremail@byupathway.edu>
2025-11-12 14:40:02 +0100

    refactor header and footer structure

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,20 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-// To create the function that will add products to cart:
-function addProductToCart(product) {
-  // Always get cart items or initialize as empty array
-  let cartItems = getLocalStorage("so-cart");
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
-  // In a condition when it's not an array, start fresh with empty array
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-  // To add new product to cart which is now an array
-  cartItems.push(product);
-  // To save updated cart back to local storage
-  setLocalStorage("so-cart", cartItems);
-}
-
-// handler for add to cart button
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// attach event listener to add to cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
