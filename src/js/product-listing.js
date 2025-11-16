af0091a55ce351c6f9bf0338f1f94cfcf33c5904
SanctaGee <tonygabito@gmail.com>
2025-11-16 22:25:09 +0100

    Created a dedicated product listing page with category parameter support

---
+++ src/js/product-listing.js
@@ -0,0 +1,17 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+// Load dynamic header/footer
+loadHeaderFooter();
+
+// Get category from URL parameter
+const category = getParam("category");
+
+// Initialize product list
+const dataSource = new ProductData();
+const listElement = document.querySelector(".product-list");
+const productList = new ProductList(category, dataSource, listElement);
+
+productList.init();
+
