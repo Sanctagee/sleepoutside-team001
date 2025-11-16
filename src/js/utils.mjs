d1efd29634b5b161a420b83a270560abcdc70a9b
SanctaGee <tonygabito@gmail.com>
2025-11-16 22:29:36 +0100

    Enhanced utility functions for API integration and template rendering

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -39,7 +39,7 @@ export function getParam(param) {
 
 export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
   const htmlStrings = list.map(template);
-  // if clear is true, we need to clear out the contents of the perent
+  // if clear is true, we need to clear out the contents of the parent element
   if (clear) {
     parentElement.innerHTML = "";
   }
