04780565dd0e8be6c5277aebf765e1bc6583a1d6
Devalasa <210102517@live.unilag.edu.ng>
2025-11-20 20:25:41 +0100

    pulled from dev branch and saw the changes

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,19 +1,21 @@
-// product-listing.js
-import ProductData from "./ProductData.mjs";
 import ExternalServices from './ExternalServices.mjs';
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
-import { filterData } from './search.js';
 
+// Load dynamic header/footer
 loadHeaderFooter();
 
+// Get category from URL parameter
 const category = getParam("category");
 
-// Load ALL data for that category
 // Initialize product list
 const dataSource = new ExternalServices();
 const listElement = document.querySelector(".product-list");
 const productList = new ProductList(category, dataSource, listElement);
+
+
+
+
 
 let allProducts = []; // store fetched data
 
@@ -34,3 +36,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   // show initial category list
   productList.renderList(allProducts);
 });
+
+
+productList.init();
