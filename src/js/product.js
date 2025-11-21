ce1a66b96317c7020f49201395d6e741dbc0f723
Emilyeme <emilyemersom395@gmail.com>
2025-11-21 02:18:27 -0800

    changes

--- src/js/product.js
+++ src/js/product.js
@@ -6,12 +6,9 @@ const dataSource = new ProductData("tents");
 // To create the function that will add products to cart:
 function addProductToCart(product) {
   // Always get cart items or initialize as empty array
-  let cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
   
-  // In a condition when it's not an array, start fresh with empty array
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
+ 
   
   // To add new product to cart which is now an array
   cartItems.push(product);
