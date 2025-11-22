2a6a64acc1d0c8865fbef9aa404b1edb054f0bc6
KarenJoos <karen.karebear@gmail.com>
2025-11-22 13:38:42 -0700

    Week 2 individual adding a discount % and badge if the price is discounted.

--- src/js/product.js
+++ src/js/product.js
@@ -11,4 +11,27 @@ const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
+const priceElem = document.querySelector(".product-price");
+const oldPriceElem = document.querySelector(".product-old-price");
+const discountElem = document.querySelector(".discount-product-badge");
+
+// Setting the final price
+priceElem.textContent = `$${product.FinalPrice}`;
+
+// If discounted
+if (product.FinalPrice < product.SuggestedRetailPrice) {
+    // shows the original price crossed out
+    oldPriceElem.textContent = `$${product.SuggestedRetailPrice}`;
+    oldPriceElem.style.textDecoration = "line-through";
+    // calculating the percentage off
+    const discountPercent = Math.round(
+        ((product.SuggestedRetailPrice - product.FinalPrice) /
+            product.SuggestedRetailPrice * 100)
+    );
+
+    // badge text
+    discountElem.textContent = `${discountPercent}% OFF`;
+    discountElem.classList.add("discount-badge");
+}
+
 loadHeaderFooter();
