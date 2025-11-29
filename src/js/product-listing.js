60e9f799261d603e358808e186f7862ccc791010
Emilyeme <emilyemersom395@gmail.com>
2025-11-29 11:25:25 -0800

    vite

---
+++ src/js/product-listing.js
@@ -0,0 +1,15 @@
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+import { loadHeaderFooter, getParam } from './utils.mjs';
+
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+
+loadHeaderFooter();
+
+const category = getParam('category') ;
+const dataSource = new ProductData();
+
+const listElement = document.querySelector('.product-list');
+const myList = new ProductList(cartegory, dataSource, listElement);
+myList.init();
